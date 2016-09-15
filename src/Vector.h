e718821589d7a0ecbcfe01f7367764be2ba67f66
trpeski <antuan.trpeski@hotmail.gr>
2016-09-15 18:18:12 +0300

    Update Vector.h

--- src/Vector.h
+++ src/Vector.h
@@ -1,5 +1,38 @@
-#ifndef Vector_h
-#define Vector_h
+#ifndef SGL_Vector_h
+#define SGL_Vector_h
+
+class SGLvector2u
+{
+    private:
+
+        uint32_t  x,y;
 
 
-#endif // Vector_h
+
+    public:
+        uint32_t get_x()
+        {
+            return x;
+        }
+        uint32_t get_y()
+        {
+            return y;
+        }
+
+        uint32_t set_x(uint32_t x_param)
+        {
+            y = x_param;
+        }
+
+        uint32_t set_y(uint32_t y_param)
+        {
+             y = y_param;
+        }
+
+
+
+};
+
+
+
+#endif // SGL_Vector_h
